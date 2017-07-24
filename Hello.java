a516ad508d88055e2da39ffba0744f22c872e0d0
mdahus <30315214+mdahus@users.noreply.github.com>
2017-07-24 18:30:32 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("Testing Docker");
 System.out.println("With java app.");
 System.out.println("compile and run java app.");
+System.out.println("new line added.");
 }
 }
