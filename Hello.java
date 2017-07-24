5c5df9bcee91a4d17b72d657c92e7d5f7bfde241
mdahus <30315214+mdahus@users.noreply.github.com>
2017-07-24 13:19:33 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+class Hello{
+public static void main(String[] args){
+System.out.println("Testing Docker");
+System.out.println("With java app.");
+System.out.println("compile and run java app.");
+}
+}
